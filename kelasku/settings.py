3dbac48bd1a12aef047566b98ca180cfa16eaccd
IceDev <102401293+IceDev2@users.noreply.github.com>
2025-08-28 00:11:05 +0700

    Update settings.py

--- kelasku/settings.py
+++ kelasku/settings.py
@@ -7,11 +7,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Env
 DEBUG = os.getenv("DEBUG", "0") == "1"
 SECRET_KEY = os.getenv("SECRET_KEY", "please-change-this")
-#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,.vercel.app").split(",")
+#ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 # Penting untuk Django 4+ saat di reverse proxy/https
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if os.getenv("CSRF_TRUSTED_ORIGINS") else []
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic', 'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
