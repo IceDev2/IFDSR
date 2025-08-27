c1b3157d3459967ba8b7d2646e0438fced991564
IceDev <102401293+IceDev2@users.noreply.github.com>
2025-08-28 00:19:01 +0700

    Update urls.py

--- kelasku/urls.py
+++ kelasku/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path, re_path
+from django.http import HttpResponse
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),  # login, logout, password reset
@@ -11,3 +14,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+def landing(_):
+    return HttpResponse("ok")  # tidak pakai template biar gak drama
+
