be52b9f3cecc9ee5878bdfa93bf30e012994765d
IceDev <102401293+IceDev2@users.noreply.github.com>
2025-08-28 00:36:35 +0700

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,13 @@
-import os
+import os, sys
+from pathlib import Path
+
+# Tambahkan ROOT repo ke sys.path supaya 'kelasku' bisa diimport
+ROOT = Path(__file__).resolve().parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kelasku.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-app = application
+app = application  # Vercel cari variabel 'app'
